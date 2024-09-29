958800084ab6089efe7095bfa30358e20f21b0ee
Benjamin Banh <benjbanh@gmail.com>
2024-09-29 16:57:03 -0700

    Publishable?

--- Personal_Website/settings.py
+++ Personal_Website/settings.py
@@ -125,7 +125,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 
